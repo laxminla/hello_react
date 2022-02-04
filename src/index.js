f8af73fd6449867bd696ef24fc952be27cfdfa3a
Laxmi Sharma <laxminla@yahoo.com>
2022-02-03 23:45:27 -0800

    added component at index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  React.createElement("div", {style: {color: "red", font: "bold", textAlign: "center"}}, React.createElement("h1", null, "Welcome To my Town!") ),
   document.getElementById('root')
 );
+ReactDOM.render( 
+  React.createElement(
+  <ul>
+  <li>Lake</li>
+  <li>Beach</li>
+  <li>Hiking</li>
+  <li>Fishing</li>
+  <li>Horse Riding</li>
+</ul>
+),
+document.getElementById('root')
+);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
